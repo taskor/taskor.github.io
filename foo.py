bfe8389211f68ecaa75cccdd9a8e00bf5245dbd7
taskor <tassbenko@gmail.com>
2025-07-16 11:16:14 -0700

    made spaces for multi-line comments between segments

--- foo.py
+++ foo.py
@@ -2,9 +2,11 @@ print("Hello World!")
 print("Your name")
 name = input("Name: ")
 print(f"Hello, {name}!")
+
 print("Your Country")
 country = input("Country: ")
-print("You are from: " + country) 
+print("You are from: " + country)
+
 print("Your number")
 n = int(input("Enter a number: "))
 if n > 0:
@@ -13,9 +15,10 @@ elif n < 0:
     print("Your number is negative.")
 else:
     print("Your number is zero.")
+
 print("Names")
 names = ["Vladimir", "Maiia", "Taisiia", "Tatiana"]
-print("The first name is:" + names[0])
-print("The second name is:" + names[1])
-print("The third name is:" + names[2])
-print("The fourth name is:" + names[3])
+print("The first name is: " + names[0])
+print("The second name is: " + names[1])
+print("The third name is: " + names[2])
+print("The fourth name is: " + names[3])
