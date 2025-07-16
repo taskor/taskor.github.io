3e830ee923bddfe6d5951358512bf0f5bcf40154
taskor <tassbenko@gmail.com>
2025-07-16 10:29:14 -0700

    made number

--- foo.py
+++ foo.py
@@ -3,3 +3,10 @@ name = input("Name: ")
 print(f"Hello, {name}!")
 country = input("Country: ")
 print("You are from:" + country) 
+n = int(input("Enter a number: "))
+if n > 0:
+    print("Your number is positive.")
+elif n < 0:
+    print("Your number is negative.")
+else:
+    print("Your number is zero.")
