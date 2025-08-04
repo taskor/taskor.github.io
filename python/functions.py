26b5e799972ef9ecab282fd740aee794d0858bbe
taskor <tassbenko@gmail.com>
2025-08-04 12:00:35 -0700

    made flight class and added to func.py)

--- python/functions.py
+++ python/functions.py
@@ -1,2 +1,40 @@
 def square(x):
     return x * x
+
+print("--------------------------------------------------------------")
+print("Object-Oriented Programming (added from main.py)")
+print("--------------------------------------------------------------")
+print("Classes")
+class Point():
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+p = Point(2, 8)
+print(p.x)
+print(p.y)
+
+class Flight():
+    def __init__ (self, capaicty):
+        self.capacity = capaicty
+        self.passengers = []
+
+    def add_passenger(self, name):
+        if not self.open_seats():
+            return False
+        
+        self.passengers.append(name)
+        return True
+
+    def open_seats(self):
+        return self.capacity - len(self.passengers)
+
+flight = Flight(3)
+
+people = ["Maiia", "Taisiia", "Tatiana", "Vladimir"]
+for person in people:
+    success = flight.add_passenger(person)
+    if success:
+        print(f"{person} has been added to the flight.")
+    else:
+        print(f"{person} could not be added to the flight due to no available seats.")
--- python/main.py
+++ python/main.py
@@ -126,3 +126,28 @@ class Point():
 p = Point(2, 8)
 print(p.x)
 print(p.y)
+
+class Flight():
+    def __init__ (self, capaicty):
+        self.capacity = capaicty
+        self.passengers = []
+
+    def add_passenger(self, name):
+        if not self.open_seats():
+            return False
+        
+        self.passengers.append(name)
+        return True
+
+    def open_seats(self):
+        return self.capacity - len(self.passengers)
+
+flight = Flight(3)
+
+people = ["Maiia", "Taisiia", "Tatiana", "Vladimir"]
+for person in people:
+    success = flight.add_passenger(person)
+    if success:
+        print(f"{person} has been added to the flight.")
+    else:
+        print(f"{person} could not be added to the flight due to no available seats.")
